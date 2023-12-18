ab0986952784b8af3c352ddc3554d428cfd10d9d
k-lila <krishnalilapalandi@gmail.com>
2023-12-18 13:14:57 -0300

    mudança de texto adicionada

--- src/scripts/togglers.js
+++ src/scripts/togglers.js
@@ -10,19 +10,35 @@ function togglerParallax() {
     }
 }
 
+function changeText(text) {
+    if (text == 'regressiva') {
+        document.getElementById('text-top').innerHTML = 'Faltam...';
+        document.getElementById('text-bot').innerHTML = 'para meu aniversário!';
+    }   else if (text == 'progressiva') {
+        document.getElementById('text-top').innerHTML = 'Passaram...';
+        document.getElementById('text-bot').innerHTML = 'desde meu aniversário!';
+    }
+}
+
 function togglerDate() {
     const current_state = document.getElementById('toggler-date').classList;
     const clock = current_state.contains('switch-btn--reg');
     if (clock) {
         current_state.add('switch-btn--prog');
         current_state.remove('switch-btn--reg');
+        changeText('progressiva')
+        
+        
     } else {
         current_state.add('switch-btn--reg');
         current_state.remove('switch-btn--prog');
+        changeText('regressiva')
+        
     }
 }
 
 export function setTogglers() {
+    changeText('regressiva')
     document.getElementById('toggler-parallax')
     .addEventListener('click', () => {
         togglerParallax();
