7ef52bb40c3e0c967a0cafe0d0b547d2c4a61b78
k-lila <krishnalilapalandi@gmail.com>
2023-12-21 01:35:08 -0300

    ajustes no parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -3,15 +3,15 @@ function parallaxMouse(event) {
     const middleY = window.innerHeight / 2;
     const xoffset = (event.clientX - middleX) / middleX;
     const yoffset = (event.clientY - middleY) / middleY;
-    const xangle = xoffset * 15;
-    const yangle = yoffset * 15;
+    const xangle = (xoffset * 15).toFixed(2);
+    const yangle = (yoffset * 15).toFixed(2);
     document.querySelector('.card').style.setProperty('--rotatex', `${yangle * -1}deg`);
     document.querySelector('.card').style.setProperty('--rotatey', `${xangle}deg`);
 }
 
 function parallaxMobile(event) {
-    const devicex = (event.beta - 30) * 0.7;
-    const devicey = event.gamma * 0.7;
+    const devicex = ((event.beta - 30) * 0.8).toFixed(2);
+    const devicey = (event.gamma * 0.8).toFixed(2);
     document.querySelector('.card').style.setProperty('--rotatex', `${devicex * -1}deg`);
     document.querySelector('.card').style.setProperty('--rotatey', `${devicey}deg`);
 }
